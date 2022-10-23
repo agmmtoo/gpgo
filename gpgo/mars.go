9ca08ed255c98f430c7d5767198f9fdd5db4e981
ammo <ammo@noobuntu.lan>
2022-10-23 16:55:42 +0630

    finish unit 1

--- gpgo/mars.go
+++ gpgo/mars.go
@@ -4,9 +4,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("My weight on the surface of Mars is ")
-	fmt.Print(149.0 * 0.3783)
-	fmt.Print(" lbs, and I would be ")
-	fmt.Print(41 * 365 / 687)
-	fmt.Print(" years old.")
+	fmt.Printf("My weight on the surface of Mars is %v lbs.\n" , 149.0 * 0.3783)
+	fmt.Printf("and I would be %v years old.\n", 41 * 365 / 687)
+
+	fmt.Printf("%-15v $%4v\n", "SpaceX", 94)
+	fmt.Printf("%-15v $%4v\n", "Virgin Galatic", 100)
 }
